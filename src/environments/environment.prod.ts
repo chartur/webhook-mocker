bf8156131b3c9bb894a9510f5c3afde60e4866c0
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2022-06-30 21:09:55 +0400

    website url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiHost: "webhook-mocker.com",
   socketUrl: "wss://webhook-mocker.com/",
-  homeSiteUrl: "http://localhost:8000"
+  homeSiteUrl: "http://webhook-mocker.me"
 };
